04861068c17131a548ddb2f1eeea29533408ed72
thoran <code@thoran.com>
2024-03-06 01:20:27 +1100

    Committed automatically by git-importall.

--- lib/month.rb
+++ lib/month.rb
@@ -1,7 +1,7 @@
 # Month
 
-# 20061004
-# 0.5.3
+# 20071216
+# 0.5.4
 
 # Description: Some code to do conversions of various formats for the representation of months.  The advantage that this has over the standard Date and Time classes is that this can handle just months and one doesn't have to specify a whole date or time in order to do the conversions.  
 
@@ -15,6 +15,8 @@
 # 4. Added self#date and self#cdate, which returns a single date based on an ordinal day, such as the 'First Monday', or 'Third Friday.  
 # 5. Added more options to self#date and self#cdate inputs and combined some equivalent ones.  
 # 6. Put in MONTH_NUMBERS_AS_STRINGS as per Todo #2.  This may save a bit of time since while there doesn't need to be any collect operation on the MONTH_NUMBERS range, there is now a to_a method applied and I'm not sure which is the more efficient.  I can change it back if need be...  
+# 3/4
+# 7. Month.dates and Month.cdates now are able to output all dates for a month by making the day argument optional.  
 
 # Todo: 
 # 1. It isn't essential that this be done, but it might be nice to acknowledge that self#cdates does not need to check whether the input day is a Fixnum or not, since it is using ISO day representation and there is no zero value as it starts at 1, it means that non-numeric strings will not convert to zero.  I could also consider using #to_str inside a begin-end block or something like that...  
@@ -53,7 +55,9 @@ class Month
   WEEK_DAY_NUMBERS = 0..6
   WEEK_DAY_NUMBERS_AS_STRINGS = '0'..'6'
   
-  def self.to_long(month = Date.today.month)
+  class << self
+  
+  def to_long(month = Date.today.month)
     if i = MONTH_NAMES_SHORT.index(month.to_s.capitalize); return MONTH_NAMES_LONG[i]
       elsif i = MONTH_NAMES_LONG.index(month.to_s.capitalize); return MONTH_NAMES_LONG[i]
       elsif i = MONTH_NUMBERS.to_a.index(month); return MONTH_NAMES_LONG[i]
@@ -62,7 +66,7 @@ class Month
     end
   end
   
-  def self.to_short(month = Date.today.month)
+  def to_short(month = Date.today.month)
     if i = MONTH_NAMES_SHORT.index(month.to_s.capitalize); return MONTH_NAMES_SHORT[i]
       elsif i = MONTH_NAMES_LONG.index(month.to_s.capitalize); return MONTH_NAMES_SHORT[i]
       elsif i = MONTH_NUMBERS.to_a.index(month); return MONTH_NAMES_SHORT[i]
@@ -71,7 +75,7 @@ class Month
     end
   end
   
-  def self.to_num(month = Date.today.month)
+  def to_num(month = Date.today.month)
     if i = MONTH_NAMES_SHORT.index(month.to_s.capitalize); return i+1
       elsif i = MONTH_NAMES_LONG.index(month.to_s.capitalize); return i+1
       elsif i = MONTH_NUMBERS.to_a.index(month); return i+1
@@ -80,11 +84,11 @@ class Month
     end
   end
   
-  def self.to_number(month = Date.today.month)
+  def to_number(month = Date.today.month)
     self.to_num(month)
   end
   
-  def self.days(month = Date.today.month, year = Date.today.year)
+  def days(month = Date.today.month, year = Date.today.year)
     if i = to_num(month)
       case e = MONTH_DAYS[i - 1]
         when Fixnum; return e
@@ -95,26 +99,32 @@ class Month
     end
   end
   
-  def self.days_in_month(month = Date.today.month, year = Date.today.year)
+  def days_in_month(month = Date.today.month, year = Date.today.year)
     self.days(month, year)
   end
   
-  def self.dates(day, month = Date.today.month, year = Date.today.year)
-    month = self.to_num(month)
-    if e = DAY_NAMES_LONG.to_a.index(day.to_s.capitalize); weekday_number = e
-      elsif e = DAY_NAMES_SHORT.to_a.index(day.to_s.capitalize); weekday_number = e 
-      elsif WEEK_DAY_NUMBERS_AS_STRINGS.to_a.member?(day); weekday_number = day.to_i
-      elsif WEEK_DAY_NUMBERS.to_a.member?(day.to_i) && day.class == Fixnum; weekday_number = day.to_i
-      else; return nil
-    end
+  def dates(day = nil, month = Date.today.month, year = Date.today.year)
     list_of_dates = []
-    Date.new(year, month, 1).upto(Date.new(year, month, self.days(month, year))) do |date|
-      list_of_dates << date.mday if date.wday == weekday_number
-    end
-    return list_of_dates
-  end
+    month = self.to_num(month)
+    unless day.nil?
+      if e = DAY_NAMES_LONG.to_a.index(day.to_s.capitalize); weekday_number = e
+        elsif e = DAY_NAMES_SHORT.to_a.index(day.to_s.capitalize); weekday_number = e 
+        elsif WEEK_DAY_NUMBERS_AS_STRINGS.to_a.member?(day); weekday_number = day.to_i
+        elsif WEEK_DAY_NUMBERS.to_a.member?(day.to_i) && day.class == Fixnum; weekday_number = day.to_i
+        else; return nil
+      end
+      Date.new(year, month, 1).upto(Date.new(year, month, self.days(month, year))) do |date|
+        list_of_dates << date.mday if date.wday == weekday_number
+      end # Date.new...upto...do
+    else
+      Date.new(year, month, 1).upto(Date.new(year, month, self.days(month, year))) do |date|
+        list_of_dates << date.mday
+      end # Date.new...upto...do
+    end # if day.nil?
+    list_of_dates
+  end # def dates
   
-  def self.day(date, month = Date.today.month, year = Date.today.year)
+  def day(date, month = Date.today.month, year = Date.today.year)
     date, month, year = date.to_i, self.to_num(month), year.to_i
     if MONTH_DAY_NUMBERS.to_a.member?(date) && MONTH_NUMBERS.to_a.member?(month) && (year.to_s =~ /\d/)
       return DAY_NAMES_LONG[Date.new(year, month, date).wday]
@@ -123,11 +133,11 @@ class Month
     end
   end
   
-  def self.day_long(date, month = Date.today.month, year = Date.today.year)
+  def day_long(date, month = Date.today.month, year = Date.today.year)
     self.day(date, month, year)
   end
   
-  def self.day_short(date, month = Date.today.month, year = Date.today.year)
+  def day_short(date, month = Date.today.month, year = Date.today.year)
     date, month, year = date.to_i, self.to_num(month), year.to_i
     if MONTH_DAY_NUMBERS.to_a.member?(date) && MONTH_NUMBERS.to_a.member?(month) && (year.to_s =~ /\d/)
       return DAY_NAMES_SHORT[Date.new(year, month, date).wday]
@@ -136,7 +146,7 @@ class Month
     end
   end
   
-  def self.wday(date, month = Date.today.month, year = Date.today.year)
+  def wday(date, month = Date.today.month, year = Date.today.year)
     date, month, year = date.to_i, self.to_num(month), year.to_i
     if MONTH_DAY_NUMBERS.to_a.member?(date) && MONTH_NUMBERS.to_a.member?(month) && (year.to_s =~ /\d/)
       return Date.new(year, month, date).wday
@@ -145,22 +155,28 @@ class Month
     end
   end
   
-  def self.cdates(day, month = Date.today.month, year = Date.today.year)
-    month = self.to_num(month)
-    if e = ISO_8601_DAY_NAMES_LONG.to_a.index(day.to_s.capitalize); weekday_number = e + 1
-      elsif e = ISO_8601_DAY_NAMES_SHORT.to_a.index(day.to_s.capitalize); weekday_number = e + 1
-      elsif ISO_8601_WEEK_DAY_NUMBERS_AS_STRINGS.to_a.member?(day); weekday_number = day.to_i
-      elsif ISO_8601_WEEK_DAY_NUMBERS.to_a.member?(day.to_i) && day.class == Fixnum; weekday_number = day.to_i
-      else; return nil
-    end
+  def cdates(day = nil, month = Date.today.month, year = Date.today.year)
     list_of_dates = []
-    Date.new(year, month, 1).upto(Date.new(year, month, self.days(month, year))) do |date|
-      list_of_dates << date.mday if date.cwday == weekday_number
-    end
-    return list_of_dates
-  end
+    unless day.nil?
+      month = self.to_num(month)
+      if e = ISO_8601_DAY_NAMES_LONG.to_a.index(day.to_s.capitalize); weekday_number = e + 1
+        elsif e = ISO_8601_DAY_NAMES_SHORT.to_a.index(day.to_s.capitalize); weekday_number = e + 1
+        elsif ISO_8601_WEEK_DAY_NUMBERS_AS_STRINGS.to_a.member?(day); weekday_number = day.to_i
+        elsif ISO_8601_WEEK_DAY_NUMBERS.to_a.member?(day.to_i) && day.class == Fixnum; weekday_number = day.to_i
+        else; return nil
+      end # if
+      Date.new(year, month, 1).upto(Date.new(year, month, self.days(month, year))) do |date|
+        list_of_dates << date.mday if date.cwday == weekday_number
+      end # Date.new...upto...do
+    else
+      Date.new(year, month, 1).upto(Date.new(year, month, self.days(month, year))) do |date|
+        list_of_dates << date.mday
+      end # Date.new...upto...do
+    end # if day.nil?
+    list_of_dates
+  end # def dates
   
-  def self.date(which_day, day, month = Date.today.month, year = Date.today.year)
+  def date(which_day, day, month = Date.today.month, year = Date.today.year)
     all = self.dates(day, month, year)
     case which_day.to_s.downcase
       when 'first', '1st', '1'; return all[0]
@@ -176,7 +192,7 @@ class Month
     end
   end
   
-  def self.cdate(which_day, day, month = Date.today.month, year = Date.today.year)
+  def cdate(which_day, day, month = Date.today.month, year = Date.today.year)
     all = self.cdates(day, month, year)
     case which_day.downcase
       when 'first', '1st', '1'; return all[0]
@@ -192,4 +208,6 @@ class Month
     end
   end
   
-end
+  end # class << self
+  
+end # class Month
