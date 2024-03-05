62addc0e5e9dea6210c4afd803814de053322695
thoran <code@thoran.com>
2024-03-06 01:20:18 +1100

    Committed automatically by git-importall.

--- lib/month.rb
+++ lib/month.rb
@@ -1,42 +1,52 @@
 class Month
-  @short_months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-  @number_months = 1..12
+  @@long_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  @@short_months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  @@number_months = 1..12
   
   def self.to_long(month)
     month = month.to_s
     case month
       when (/\w/ && /\D/)
-        case month.downcase
-        when 'jan', 'january'; long_month = 'January'
-        when 'feb', 'february'; long_month = 'February'
-        when 'mar', 'march'; long_month = 'March'
-        when 'apr', 'april'; long_month = 'April'
-        when 'may'; long_month = 'May'
-        when 'jun', 'june'; long_month = 'June'
-        when 'jul', 'july'; long_month = 'July'
-        when 'aug', 'august'; long_month = 'August'
-        when 'sep', 'september'; long_month = 'September'
-        when 'oct', 'october'; long_month = 'October'
-        when 'nov', 'november'; long_month = 'November'
-        when 'dec', 'december'; long_month = 'December'
+        if (@@short_months.to_a.member?(month) || @@long_months.to_a.member?(month))
+          case month.downcase
+            when 'jan', 'january'; long_month = 'January'
+            when 'feb', 'february'; long_month = 'February'
+            when 'mar', 'march'; long_month = 'March'
+            when 'apr', 'april'; long_month = 'April'
+            when 'may'; long_month = 'May'
+            when 'jun', 'june'; long_month = 'June'
+            when 'jul', 'july'; long_month = 'July'
+            when 'aug', 'august'; long_month = 'August'
+            when 'sep', 'september'; long_month = 'September'
+            when 'oct', 'october'; long_month = 'October'
+            when 'nov', 'november'; long_month = 'November'
+            when 'dec', 'december'; long_month = 'December'
+          end
+          return long_month
+        else
+          return nil
         end
-        return long_month
+      end
       when (/\w/ && /\d/)
-        case month
-          when '1'; long_month = 'January'
-          when '2'; long_month = 'February'
-          when '3'; long_month = 'March'
-          when '4'; long_month = 'April'
-          when '5'; long_month = 'May'
-          when '6'; long_month = 'June'
-          when '7'; long_month = 'July'
-          when '8'; long_month = 'August'
-          when '9'; long_month = 'September'
-          when '10'; long_month = 'October'
-          when '11'; long_month = 'November'
-          when '12'; long_month = 'December'
+        if @@number_months.to_a.member?(month.to_i)
+          case month
+            when '1'; long_month = 'January'
+            when '2'; long_month = 'February'
+            when '3'; long_month = 'March'
+            when '4'; long_month = 'April'
+            when '5'; long_month = 'May'
+            when '6'; long_month = 'June'
+            when '7'; long_month = 'July'
+            when '8'; long_month = 'August'
+            when '9'; long_month = 'September'
+            when '10'; long_month = 'October'
+            when '11'; long_month = 'November'
+            when '12'; long_month = 'December'
+          end
+          return long_month
+        else
+          return nil
         end
-        return long_month
       else
         return nil
       end
@@ -46,37 +56,45 @@ class Month
     month = month.to_s
     case month
       when (/\w/ && /\D/) 
-        case month.downcase
-          when 'jan', 'january'; short_month = 'Jan'
-          when 'feb', 'february'; short_month = 'Feb'
-          when 'mar', 'march'; short_month = 'Mar'
-          when 'apr', 'april'; short_month = 'Apr'
-          when 'may'; short_month = 'May'
-          when 'june'; short_month = 'Jun'
-          when 'july'; short_month = 'Jul'
-          when 'august'; short_month = 'Aug'
-          when 'september'; short_month = 'Sep'
-          when 'october'; short_month = 'Oct'
-          when 'november'; short_month = 'Nov'
-          when 'december'; short_month = 'Dec'
+        if (@@short_months.to_a.member?(month) || @@long_months.to_a.member?(month))
+          case month.downcase
+            when 'jan', 'january'; short_month = 'Jan'
+            when 'feb', 'february'; short_month = 'Feb'
+            when 'mar', 'march'; short_month = 'Mar'
+            when 'apr', 'april'; short_month = 'Apr'
+            when 'may'; short_month = 'May'
+            when 'june'; short_month = 'Jun'
+            when 'july'; short_month = 'Jul'
+            when 'august'; short_month = 'Aug'
+            when 'september'; short_month = 'Sep'
+            when 'october'; short_month = 'Oct'
+            when 'november'; short_month = 'Nov'
+            when 'december'; short_month = 'Dec'
+          end
+          return short_month
+        else
+          return nil
         end
-        return short_month
       when (/\w/ && /\d/)
-        case month
-          when '1'; short_month = 'Jan'
-          when '2'; short_month = 'Feb'
-          when '3'; short_month = 'Mar'
-          when '4'; short_month = 'Apr'
-          when '5'; short_month = 'May'
-          when '6'; short_month = 'Jun'
-          when '7'; short_month = 'Jul'
-          when '8'; short_month = 'Aug'
-          when '9'; short_month = 'Sep'
-          when '10'; short_month = 'Oct'
-          when '11'; short_month = 'Nov'
-          when '12'; short_month = 'Dec'
+        if @@number_months.to_a.member?(month.to_i)
+          case month
+            when '1'; short_month = 'Jan'
+            when '2'; short_month = 'Feb'
+            when '3'; short_month = 'Mar'
+            when '4'; short_month = 'Apr'
+            when '5'; short_month = 'May'
+            when '6'; short_month = 'Jun'
+            when '7'; short_month = 'Jul'
+            when '8'; short_month = 'Aug'
+            when '9'; short_month = 'Sep'
+            when '10'; short_month = 'Oct'
+            when '11'; short_month = 'Nov'
+            when '12'; short_month = 'Dec'
+          end
+          return short_month
+        else
+          return nil
         end
-        return short_month
       else
         return nil
       end
@@ -87,23 +105,27 @@ class Month
     month = month.to_s
     case month
       when (/\w/ && /\D/) 
-        case month.downcase
-          when 'jan', 'january'; number_month = 1
-          when 'feb', 'february'; number_month = 2
-          when 'mar', 'march'; number_month = 3
-          when 'apr', 'april'; number_month = 4
-          when 'may'; number_month = 5
-          when 'jun', 'june'; number_month = 6
-          when 'jul', 'july'; number_month = 7
-          when 'aug', 'august'; number_month = 8
-          when 'sep', 'september'; number_month = 9
-          when 'oct', 'october'; number_month = 10
-          when 'nov', 'november'; number_month = 11
-          when 'dec', 'december'; number_month = 12
+        if (@@short_months.to_a.member?(month) || @@long_months.to_a.member?(month))
+          case month.downcase
+            when 'jan', 'january'; number_month = 1
+            when 'feb', 'february'; number_month = 2
+            when 'mar', 'march'; number_month = 3
+            when 'apr', 'april'; number_month = 4
+            when 'may'; number_month = 5
+            when 'jun', 'june'; number_month = 6
+            when 'jul', 'july'; number_month = 7
+            when 'aug', 'august'; number_month = 8
+            when 'sep', 'september'; number_month = 9
+            when 'oct', 'october'; number_month = 10
+            when 'nov', 'november'; number_month = 11
+            when 'dec', 'december'; number_month = 12
+          end
+          return number_month
+        else
+          return nil
         end
-        return number_month
       when (/\w/ && /\d/)
-        if @number_months.to_a.member?(month)
+        if @@number_months.to_a.member?(month.to_i)
           return month.to_i
         else
           return nil
@@ -113,7 +135,7 @@ class Month
       end
   end
   
-  def days(month year = Date.)
+  def days(month, year = Date.today.year)
     case month
       when '1'; days = 31
       when '2'; days = 28  # Need to check for leap years; and should I bother with millenia?
