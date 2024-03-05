767fbdbb4cecf6695e0383e177a3add426f36fdf
thoran <code@thoran.com>
2024-03-06 01:20:37 +1100

    Committed automatically by git-importall.

--- Month.rbl/test/ts_Month.rb
+++ Month.rbl/test/ts_Month.rb
@@ -1,16 +1,14 @@
 # test/ts_Month.rb
 
-gem 'minitest'
 require 'minitest/autorun'
-
-lib_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
-
-require File.join(lib_dir, 'Date', 'to_month.rb')
-require File.join(lib_dir, 'Month', '_all.rb')
 
 test_dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift(test_dir) unless $LOAD_PATH.include?(test_dir)
+
+lib_dir = File.expand_path(File.join(test_dir, '..', 'lib'))
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
+require File.join("#{lib_dir}", 'Month.rb')
 
 tests = Dir[File.join(test_dir, '*.rb')].reject!{|file| file == File.expand_path(__FILE__)}
 tests.each{|test| require test}
